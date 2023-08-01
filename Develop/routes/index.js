fbd854e1d8106b465c1b83531319555912a2d57a
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-08-01 16:47:55 -0500

    index.js complete

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -1,3 +1,15 @@
 // TODO -- Bring in express module
 // TODO -- call /db/notes
 // TODO -- set app to 
+
+const express = require('express');
+
+
+// Import our modular routers for /tips and /feedback
+const notesRouter = require('./notes');
+
+const app = express();
+
+app.use('/notes', notesRouter);
+
+module.exports = app;
